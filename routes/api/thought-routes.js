aa2e92f3f3970eb0f21c96e64bf3ed21bbd991d9
DMartin <davidmartin8127@gmail.com>
2023-03-10 19:07:26 -0800

    initial commit

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,50 @@
+
+const router = require('express').Router();
+const { Thought, Reaction } = require('../../models')
+
+//TODO: ROUTE TO GET ALL THOUGHTS
+router.get('/', (req, res) => {
+    Thought.find({}, (err, thoughts) => {
+        res.status(200).json(thoughts)
+    })
+})
+
+//TODO: ROUTE TO CREATE A NEW THOUGHT
+router.post('/', (req, res) => {
+    Thought.create({
+        text: req.body.thoughtText,
+    }, (err, thought) => {
+        if (err) {
+            res.status(500).json(err)
+        } else {
+            res.status(200).json(true)
+        }
+    })
+});
+
+//TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
+router.get('/:thoughtId', (req, res) => {
+
+})
+
+//TODO: ROUTE TO UPDATE A THOUGHT
+router.put('/', (req, res) => {
+
+})
+
+//TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
+router.delete('/:thoughtId', (req, res) => {
+
+});
+
+//TODO: ROUTE TO ADD REACTION TO A THOUGHT
+router.post('/:thoughtId/reactions', (req, res) => {
+
+});
+
+//TODO: ROUTE TO DELETE A REACTION ON A THOUGHT
+router.delete('/:thoughtId/reactions/:reactionId', (req, res) => {
+
+})
+
+module.exports = router;
