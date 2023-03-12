f075e7382463260a7918f72e652a1a2c6fd662dc
DMartin <davidmartin8127@gmail.com>
2023-03-11 16:20:14 -0800

    added to thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -12,7 +12,8 @@ router.get('/', (req, res) => {
 //TODO: ROUTE TO CREATE A NEW THOUGHT
 router.post('/', (req, res) => {
     Thought.create({
-        text: req.body.thoughtText,
+        thoughtText: req.body.thoughtText,
+        username: req.body.username
     }, (err, thought) => {
         if (err) {
             res.status(500).json(err)
@@ -24,22 +25,43 @@ router.post('/', (req, res) => {
 
 //TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
 router.get('/:thoughtId', (req, res) => {
-
-})
+    Thought.findById(req.params.thoughtId, (err, thought) => {
+        if (err) {
+            res.status(500).json(err)
+        } else {
+            res.status(200).json(thought)
+        }
+    })
+});
 
 //TODO: ROUTE TO UPDATE A THOUGHT
-router.put('/', (req, res) => {
-
+router.put('/:thoughtId', (req, res) => {
+    Thought.findByIdAndUpdate(req.params.thoughtId, {
+        thoughtText: req.body.thoughtText,
+        username: req.body.username,
+    }, (err, thought) => {
+        if (err) {
+            res.status(500).json(err)
+        } else {
+            res.status(200).send( {message: "Successful Update!"} )
+        }
+    })
 })
 
 //TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
 router.delete('/:thoughtId', (req, res) => {
-
+    Thought.findByIdAndDelete(req.params.thoughtId, (err, thought) => {
+        if (err) {
+            res.status(500).json(err)
+        } else {
+            res.status(200).send( {message: "Deleted successfully!"} )
+        }
+    })
 });
 
 //TODO: ROUTE TO ADD REACTION TO A THOUGHT
 router.post('/:thoughtId/reactions', (req, res) => {
-
+    
 });
 
 //TODO: ROUTE TO DELETE A REACTION ON A THOUGHT
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,17 +4,28 @@ const {User} = require("../../models")
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
 router.get('/', (req,res)=> {
-
+    User.find({}, (err, users) => {
+        res.status(200).json(users)
+    })
 })
 
 //TODO - ROUTE THAT CREATES A NEW USER
 router.post('/', (req,res)=> {
-
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+    }, (err, user) => {
+        if (err) {
+            res.status(500).json(err)
+        } else {
+            res.status(200).json(true)
+        }
+    })
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
 router.get('/:userId', (req,res) => {
-
+    
 })
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
